c88f9545c2bda5998cb57a768ab731e9b30477d8
brunocoronha <brunocoronha@gmail.com>
2023-10-24 09:07:49 -0300

    remocao veiculo

--- src/main/java/com/mycompany/deliverycontrol/repository/Banco.java
+++ src/main/java/com/mycompany/deliverycontrol/repository/Banco.java
@@ -33,27 +33,23 @@ public class Banco {
         connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/DeliveryControl", user, senha);
     }
 
-    public boolean estaConectado() throws SQLException {
-        if (connection.isClosed()) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
     public void fechaConexao() throws SQLException {
         connection.close();
     }
 
-    //#region CRUD_ENTREGADOR
+    public boolean estaConectado() throws SQLException {
+        return connection.isClosed();
+    }
+
+    // #region CRUD_ENTREGADOR
     public void insertEntregador(Entregador entregador) throws SQLException {
         System.out.println(entregador.toString());
-        String sql = "INSERT INTO entregador (nome, telefone) VALUES (?, ?)";        
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);        
+        String sql = "INSERT INTO entregador (nome, telefone) VALUES (?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setString(1, entregador.getNome());
-        preparedStatement.setString(2, entregador.getTelefone());        
+        preparedStatement.setString(2, entregador.getTelefone());
         preparedStatement.executeUpdate();
-        
+
     }
 
     public boolean removeEntregador(Integer id) throws SQLException {
@@ -83,6 +79,17 @@ public class Banco {
         return entregadores;
     }
 
+    public Entregador buscaEntregador(Integer id) throws SQLException {
+        String sql = "SELECT * FROM entregador WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, id);
+        ResultSet resultset = preparedStatement.executeQuery();
+        int idEntregador = resultset.getInt("id");
+        String nomeEntregador = resultset.getString("nome");
+        String telefoneEntregador = resultset.getString("telefone");
+        return new Entregador(idEntregador, nomeEntregador, telefoneEntregador);
+    }
+
     public boolean updateEntregador(Entregador entregador) throws SQLException {
         String sql = "UPDATE entregador SET nome = ?, telefone = ? WHERE id = ?";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
@@ -95,16 +102,9 @@ public class Banco {
         return true;
     }
 
-    public Entregador buscaEntregador(Integer id) throws SQLException {
-        String sql = "SELECT * FROM entregador WHERE id = ?";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        preparedStatement.setInt(1, id);
-        ResultSet resultset = preparedStatement.executeQuery();
-        int idEntregador = resultset.getInt("id");
-        String nomeEntregador = resultset.getString("nome");
-        String telefoneEntregador = resultset.getString("telefone");
-        return new Entregador(idEntregador, nomeEntregador, telefoneEntregador);
-    }
+    // #endregion
+
+    //#region CRUD_CLIENTE
 
     public void insertCliente(Cliente cliente) throws SQLException {
         String sql = "INSERT INTO cliente (nome, endereco, telefone) VALUES (?, ?, ?)";
@@ -114,6 +114,58 @@ public class Banco {
         preparedStatement.setString(3, cliente.getTelefone());
         preparedStatement.executeUpdate();
     }
-    //#endregion
 
+    public boolean removeCliente(Integer id) throws SQLException {
+        String sql = "DELETE FROM cliente WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, id);
+        System.out.println(sql);
+        if (preparedStatement.executeUpdate() < 1) {
+            return false;
+        }
+        return true;
+    }
+
+    public ArrayList<Cliente> buscaClientes() throws SQLException {
+        ArrayList<Cliente> clientes = new ArrayList<>();
+        String sql = "SELECT id, nome, endereco, telefone FROM cliente";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            Integer id = resultSet.getInt("id");
+            String nome = resultSet.getString("nome");
+            String endereco = resultSet.getString("endereco");
+            String telefone = resultSet.getString("telefone");
+            Cliente cliente = new Cliente(id, nome, endereco, telefone);
+            clientes.add(cliente);
+        }
+        return clientes;
+    }
+
+    public Cliente buscaCliente(Integer id) throws SQLException {
+        String sql = "SELECT * FROM cliente WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setInt(1, id);
+        ResultSet resultset = preparedStatement.executeQuery();
+        int idCliente = resultset.getInt("id");
+        String nomeCliente = resultset.getString("nome");
+        String enderecoCLiente = resultset.getString("endereco");
+        String telefoneCliente = resultset.getString("telefone");
+        return new Cliente(idCliente, nomeCliente, enderecoCLiente, telefoneCliente);
+    }
+
+    public boolean updateCliente(Cliente cliente) throws SQLException {
+        String sql = "UPDATE cliente SET nome = ?, endereco = ?, telefone = ? WHERE id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, cliente.getNome());
+        preparedStatement.setString(2, cliente.getEndereco());
+        preparedStatement.setString(3, cliente.getTelefone());
+        preparedStatement.setInt(4, cliente.getId());
+        if (preparedStatement.executeUpdate() < 1) {
+            return false;
+        }
+        return true;
+    }
+
+    //#endregion
 }
